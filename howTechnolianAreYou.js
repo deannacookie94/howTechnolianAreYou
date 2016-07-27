ad16ffd4e533acdfc6d8c896e39d062cfd92e46c
Deanna <dc13c@my.fsu.edu>
2016-07-27 22:45:39 +0000

    How Technolian are You first commit

---
+++ howTechnolianAreYou.js
@@ -0,0 +1,48 @@
+
+function clicked () {
+	var hideUsername = document.getElementById("username").style.display = "none"; 
+	var hideSubmit = document.getElementById("submit").style.display = "none"; 
+	var hideInputBox = document.getElementById("enterUsername").style.display = "none";
+	var displayInstuctions = document.getElementById("instructions").style.display = "block";
+	var displayOkayButton = document.getElementById("okay").style.display = "block";
+}
+
+function question1 () {
+	var hideInstuctions = document.getElementById("instructions").style.display = "none";
+	var hideOkayButton = document.getElementById("okay").style.display = "none";
+	var displayQuestion1 = document.getElementById("question1").style.display = "block";
+}
+
+function question2 () {
+	var hideInstuctions = document.getElementById("instructions").style.display = "none";
+	var hideOkayButton = document.getElementById("okay").style.display = "none";
+	var hideQuestion1 = document.getElementById("question1").style.display = "none";
+	var displayQuestion2 = document.getElementById("question2").style.display = "block";
+}
+
+function question3 () {
+	var hideInstuctions = document.getElementById("instructions").style.display = "none";
+	var hideOkayButton = document.getElementById("okay").style.display = "none";
+	var hideQuestion1 = document.getElementById("question1").style.display = "none";
+	var displayQuestion2 = document.getElementById("question2").style.display = "none";
+	var displayQuestion3 = document.getElementById("question3").style.display = "block";
+}
+
+function question4 () {
+	var hideInstuctions = document.getElementById("instructions").style.display = "none";
+	var hideOkayButton = document.getElementById("okay").style.display = "none";
+	var hideQuestion1 = document.getElementById("question1").style.display = "none";
+	var displayQuestion2 = document.getElementById("question2").style.display = "none";
+	var displayQuestion3 = document.getElementById("question3").style.display = "none";
+	var displayQuestion4 = document.getElementById("question4").style.display = "block";
+}
+
+function question5 () {
+	var hideInstuctions = document.getElementById("instructions").style.display = "none";
+	var hideOkayButton = document.getElementById("okay").style.display = "none";
+	var hideQuestion1 = document.getElementById("question1").style.display = "none";
+	var displayQuestion2 = document.getElementById("question2").style.display = "none";
+	var displayQuestion3 = document.getElementById("question3").style.display = "none";
+	var displayQuestion4 = document.getElementById("question4").style.display = "none";
+	var displayQuestion5 = document.getElementById("question5").style.display = "block";
+}
